7256318e710e85ae01736adfa24128471d395f04
goshima5 <goshima5@gmail.com>
2018-02-03 17:27:37 +0900

    init base app for pi server

---
+++ pi-sever/routes/api.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var router = express.Router();
+var fs = require('fs');
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  // 送信例    curl http://IP/api?cmd=hogehoge
+  var filePath = '/tmp/led';
+  var data = req.query.cmd;
+  res.send('data was ' + data);
+  fs.writeFile(filePath, data, function(err) {
+    if(err) {
+      return console.log(err);
+    }
+    console.log("api ok");
+  });
+});
+
+module.exports = router;
+
+
+
+
+
+
+
+
+
